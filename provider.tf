61758ebf7cbc70267c00e140c0001ee2401424e4
SivaRoopa <37268753+SivaRoopa@users.noreply.github.com>
2022-12-12 19:18:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  access_key              = "AKIAQABVEF4Y7FIIOQUY"
-  secret_key              = "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"
   profile                 = "default"
   region                  = "us-east-1"
 }
